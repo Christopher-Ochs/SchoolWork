473437726f55bc65b377aa297504e8496fec3351
Christopher-Ochs <ochscs@mail.uc.edu>
2016-10-19 21:59:12 -0400

    Add files via upload

---
+++ sudoku6.py
@@ -0,0 +1,178 @@
+# sudoku6.py CS2028 2016 Cheng
+# tries to solve sudoku puzzles using rule1 and rule2 repeatedly
+# then depth-first search is used with a stack
+from collections import *
+import copy, linkedliststackqueue2
+
+def readPuzzle():
+	matrix = []
+	filename = input("Please enter a Sudoku puzzle file name: ")
+	file = open(filename, "r")
+	for line in file:
+		row = []
+		for i in range(9):
+			s = line[i * 2]
+			if s == 'x':
+				row.append(set(['1','2','3','4','5','6','7','8','9']))
+			else:
+				row.append(set([s]))
+		matrix.append(row)
+	file.close()
+	return matrix
+
+def getGroups(matrix):
+	groups = []
+	for i in range(9):
+		group = []
+		for j in range(9):
+			group.append(matrix[i][j])
+		groups.append(group)
+	for i in range(9):
+		group = []
+		for j in range(9):
+			group.append(matrix[j][i])
+		groups.append(group)
+	for i in range(3):
+		for j in range(3):
+			group = []
+			for k in range(3):
+				for l in range(3):
+					group.append(matrix[i * 3 + k][j * 3 + l])
+			groups.append(group)
+	return groups
+
+def rule1(group):
+	changed = 0
+	for cell in group:
+		count = 1
+		for cell2 in group:
+			if cell2 != cell and cell2 <= cell:
+				count += 1
+		if count == len(cell):
+			for cell2 in group:
+				if not cell2 <= cell and not cell2.isdisjoint(cell):
+					cell2 -= cell
+					changed = 1
+		elif count > len(cell):
+			return -1
+		else:
+			pass
+	return changed
+
+def rule2(group):
+	changed = 0
+	cnt = Counter()
+	for cell in group:
+		cnt += Counter(cell)
+	for item in cnt:
+		if cnt[item] == 1:
+			for cell in group:
+				if item in cell and len(cell) > 1:
+					cell.clear()
+					cell.add(item)
+					changed = 1
+	return changed
+
+def reduceGroup(group):
+	changed = 0
+	ret = rule1(group) 
+	if ret == 1:
+		changed = 1
+	elif ret == -1:
+		return -1
+	if rule2(group) == 1 and changed == 0:
+		changed = 1
+	return changed
+
+def showMatrix(matrix):
+	for i in range(9):
+		for j in range(9):
+			if len(matrix[i][j]) > 1:
+				print('x', end=' ')
+			elif len(matrix[i][j]) == 0:
+				print(' ', end=' ')
+			else:
+				item = matrix[i][j].pop()
+				matrix[i][j].add(item)
+				print(item, end=' ')
+		print()
+	print()		
+
+def reduceGroups(groups):
+	changed = 0
+	for group in groups:
+		ret = reduceGroup(group)
+		if ret == 1:
+			changed = 1
+		elif ret == -1:
+			return -1
+	return changed
+
+def reduce(matrix):
+	changed = 1
+	groups = getGroups(matrix)
+	while changed == 1:
+		changed = reduceGroups(groups)
+	if changed == -1:
+		return -1
+	else:
+		return 0
+
+
+def solutionViable(matrix):
+	for i in range(9):
+		for j in range(9):
+			if len(matrix[i][j]) == 0:
+				return False
+	return True
+
+def solutionOK(matrix):
+	for i in range(9):
+		for j in range(9):
+			if len(matrix[i][j]) > 1:
+				return False
+	return True
+
+def solve(matrix):
+	queue = linkedliststackqueue2.Queue()
+	
+	queue.enqueue(matrix)
+	while queue.isEmpty() != True:
+		m = queue.dequeue()
+		ret = reduce(m)
+		if ret == -1:
+			continue
+		if not solutionViable(m):
+			continue
+		if solutionOK(m):
+			return m
+		print("Searching...")
+		showMatrix(m)
+		for i in range(9):
+			found = False
+			for j in range(9):
+				if len(m[i][j]) > 1:
+					found = True
+					break
+			if found:
+				break
+		print(len(m[i][j]), 'guesses at row', i, 'column', j)
+		print(m[i][j])
+		for k in m[i][j]:
+			mcopy = copy.deepcopy(m)
+			mcopy[i][j] = set([k])
+			queue.enqueue(mcopy)
+	return None
+		
+def main():
+	matrix = readPuzzle()
+	showMatrix(matrix)
+	print("Begin Solving")
+	matrix = solve(matrix)
+	if matrix == None:
+		print("No Solution Found!!!")
+		return
+	showMatrix(matrix)
+
+if __name__ == "__main__":
+	main()

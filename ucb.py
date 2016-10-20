5b3f0581557cf1bebe929c66f9953bec33f1fdf9
Christopher-Ochs <ochscs@mail.uc.edu>
2016-10-19 22:33:30 -0400

    Add files via upload

--- ucb.py
+++ ucb.py
@@ -3,7 +3,6 @@
 import code
 import functools
 import inspect
-import re
 import signal
 import sys
 
@@ -55,8 +54,7 @@ def trace(fn):
 
 def log(message):
     """Print an indented message (used with trace)."""
-    if type(message) is not str:
-        message = str(message)
+    message = str(message)
     print(_PREFIX + re.sub('\n', '\n' + _PREFIX, message))
 
 
@@ -72,16 +70,11 @@ def interact(msg=None):
     On Unix:
       <Control>-D exits the interactive session and returns to normal execution.
     In Windows:
-      <Control>-Z <Enter> exists the interactive session and returns to normal
+      <Control>-Z <Enter> exits the interactive session and returns to normal
       execution.
     """
-    # use exception trick to pick up the current frame
-    try:
-        raise None
-    except:
-        frame = sys.exc_info()[2].tb_frame.f_back
-
     # evaluate commands in current namespace
+    frame = inspect.currentframe().f_back
     namespace = frame.f_globals.copy()
     namespace.update(frame.f_locals)
 
